abc09cb4bbbf53228b5b6f536603c7350fd20698
AwesomeJim <jimnahgua@gmail.com>
2023-09-22 18:57:37 +0300

    -Configured remote and default Repository

--- app/src/main/java/com/awesomejim/weatherforecast/di/data/RepositoryModule.kt
+++ app/src/main/java/com/awesomejim/weatherforecast/di/data/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.awesomejim.weatherforecast.di.data
 
+import com.awesomejim.weatherforecast.data.DefaultWeatherRepository
+import com.awesomejim.weatherforecast.data.WeatherRepository
 import com.awesomejim.weatherforecast.data.source.remote.DefaultRemoteWeatherDataSource
 import com.awesomejim.weatherforecast.data.source.remote.RemoteDataSource
 import dagger.Binds
@@ -10,6 +12,10 @@ import dagger.hilt.android.components.ViewModelComponent
 @Module
 @InstallIn(ViewModelComponent::class)
 interface RepositoryModule {
+
+    @Binds
+    fun bindWeatherRepository(weatherRepository: DefaultWeatherRepository): WeatherRepository
+
     @Binds
     fun bindRemoteWeatherDataSource(remoteWeatherDataSource: DefaultRemoteWeatherDataSource): RemoteDataSource
 }
